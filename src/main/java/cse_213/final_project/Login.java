08e94a00482d4f207db3d9b94ca827f5fec8395a
TAWHID1510 <tawhidmoslim245@gmail.com>
2025-04-03 22:19:21 +0600

    logIn FXML and login controller class created

---
+++ src/main/java/cse_213/final_project/Login.java
@@ -0,0 +1,21 @@
+package cse_213.final_project;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login
+{
+    @javafx.fxml.FXML
+    private TextField userIDtf;
+    @javafx.fxml.FXML
+    private PasswordField passwordTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginOA(ActionEvent actionEvent) {
+    }
+}
