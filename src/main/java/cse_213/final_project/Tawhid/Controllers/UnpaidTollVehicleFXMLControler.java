0fd01f1256606458b9611b4ccf53336aef6945bb
TAWHID1510 <tawhidmoslim245@gmail.com>
2025-04-07 23:39:14 +0600

    Unpaid toll vehicle FXML designed and controller class created

---
+++ src/main/java/cse_213/final_project/Tawhid/Controllers/UnpaidTollVehicleFXMLControler.java
@@ -0,0 +1,32 @@
+package cse_213.final_project.Tawhid.Controllers;
+
+import javafx.event.*;
+import javafx.scene.control.*;
+
+public class UnpaidTollVehicleFXMLControler
+{
+    @javafx.fxml.FXML
+    private TextField carRegNOTF;
+    @javafx.fxml.FXML
+    private DatePicker ruleBreakDateDP;
+    @javafx.fxml.FXML
+    private TextField tollAmountTF;
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private ComboBox vehicleTypeCB;
+    @javafx.fxml.FXML
+    private TableColumn regNoCol;
+    @javafx.fxml.FXML
+    private TableColumn tollAmountCol;
+    @javafx.fxml.FXML
+    private TableColumn vehicleTypeCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerOA(ActionEvent actionEvent) {
+    }
+}
