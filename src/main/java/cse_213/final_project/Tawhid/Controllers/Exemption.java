eec4f4dbcd44b55fbc346a58f6940261047db581
TAWHID1510 <tawhidmoslim245@gmail.com>
2025-04-10 02:56:56 +0600

    exemption controller class created and FXML File designed

---
+++ src/main/java/cse_213/final_project/Tawhid/Controllers/Exemption.java
@@ -0,0 +1,29 @@
+package cse_213.final_project.Tawhid.Controllers;
+
+public class Exemption
+{
+    @javafx.fxml.FXML
+    private TableColumn exemptionVehicleTypeCol;
+    @javafx.fxml.FXML
+    private TableView exemptionTableView;
+    @javafx.fxml.FXML
+    private TableColumn exemptiondrivingLicenceNoCol;
+    @javafx.fxml.FXML
+    private TableColumn exemptionReasonCol;
+    @javafx.fxml.FXML
+    private TableColumn exemptionRegNoCol;
+    @javafx.fxml.FXML
+    private TableColumn exemptionDateCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectExemptionOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acceptExemptionOA(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/cse_213/final_project/Tawhid/Controllers/ManageExemptionController.java
+++ src/main/java/cse_213/final_project/Tawhid/Controllers/ManageExemptionController.java
@@ -3,5 +3,23 @@ package cse_213.final_project.Tawhid.Controllers;
 public class ManageExemptionController
 {
     @javafx.fxml.FXML
+    private TextField manageExemptionReasonTF;
+    @javafx.fxml.FXML
+    private Label errorlabelmanageExemption;
+    @javafx.fxml.FXML
+    private TextField manageExemptionVehiRegNoTF;
+    @javafx.fxml.FXML
+    private TextField manageExemptionDrivingLicenceTF;
+    @javafx.fxml.FXML
+    private DatePicker manageExemptionTravelDP;
+    @javafx.fxml.FXML
+    private Label regNoValidationmassagelabelManageEXMP;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void ExemptionOA(ActionEvent actionEvent) {
+    }
+}
