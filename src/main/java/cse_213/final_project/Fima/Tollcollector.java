a0b9cf14fda4ebd03a10bf035120781c738e8da0
anikafima <2321439@iub.edu.bd>
2025-04-11 23:14:32 -0400

    fima's create tollcollector fxml

---
+++ src/main/java/cse_213/final_project/Fima/Tollcollector.java
@@ -0,0 +1,20 @@
+package cse_213.final_project.Fima;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Tollcollector
+{
+    @javafx.fxml.FXML
+    private TextField regnumtextfield;
+    @javafx.fxml.FXML
+    private TextField nametextfield;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+}
