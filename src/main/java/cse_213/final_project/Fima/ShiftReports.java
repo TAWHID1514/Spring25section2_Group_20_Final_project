2a1f0aa5a630e84210b95fbc15665aa42a695168
anikafima <2321439@iub.edu.bd>
2025-04-13 20:38:36 -0400

    create  .fxml

---
+++ src/main/java/cse_213/final_project/Fima/ShiftReports.java
@@ -0,0 +1,7 @@
+package cse_213.final_project.Fima;
+
+public class ShiftReports
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/cse_213/final_project/Fima/TollBoothOperatorDashboardFima.java
@@ -0,0 +1,43 @@
+package cse_213.final_project.Fima;
+
+public class TollBoothOperatorDashboardFima
+{
+    @javafx.fxml.FXML
+    private Label TollBoothOperatorDashboardlable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void shiftattendanceonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void technicalissuesonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void informationandsupportonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void vehicleinformationOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void collecttollpaymentsonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void profileinformationonactionbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void vipemergencyvehiclesonactionbutton(ActionEvent actionEvent) {
+    }
+}
