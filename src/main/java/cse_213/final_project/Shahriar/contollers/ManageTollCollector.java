d1ccfd33854f1293b361983e372f6754cbcd5b28
sayem560 <sayemmr04@gmail.com>
2025-04-15 02:51:22 +0600

    mangetollCollector of cotroller and fxml design

---
+++ src/main/java/cse_213/final_project/Shahriar/contollers/ManageTollCollector.java
@@ -0,0 +1,33 @@
+package cse_213.final_project.Shahriar.contollers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageTollCollector
+{
+    @javafx.fxml.FXML
+    private TextField Namefield;
+    @javafx.fxml.FXML
+    private ComboBox rolebox;
+    @javafx.fxml.FXML
+    private TableView collectorTable;
+    @javafx.fxml.FXML
+    private TableColumn namecolum;
+    @javafx.fxml.FXML
+    private TableColumn rolecolum;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRemoveCollectoronaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddCollectoronaction(ActionEvent actionEvent) {
+    }
+}
