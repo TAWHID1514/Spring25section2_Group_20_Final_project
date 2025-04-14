913efcbcc0ba1520ceebaf963f190930a5b32725
mehrab <mehrabbhuiyan123@gmail.com>
2025-04-14 23:59:09 +0600

    up

---
+++ src/main/java/cse_213/final_project/Mehrab/FinancialRecommendationController.java
@@ -0,0 +1,35 @@
+package cse_213.final_project.Mehrab;
+
+public class FinancialRecommendationController
+{
+    @javafx.fxml.FXML
+    private TextField recommendationTitleField;
+    @javafx.fxml.FXML
+    private DatePicker projectTimelineDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox recommendationTypeComboBox;
+    @javafx.fxml.FXML
+    private TextField budgetAllocationField;
+    @javafx.fxml.FXML
+    private Label errorMessage;
+    @javafx.fxml.FXML
+    private TextArea recommendationDescriptionField;
+    @javafx.fxml.FXML
+    private TextArea recommendationDataArea;
+    @javafx.fxml.FXML
+    private TextField financialAmountField;
+    @javafx.fxml.FXML
+    private DatePicker recommendationDatePicker;
+    @javafx.fxml.FXML
+    private Button sendRecommendationButton;
+    @javafx.fxml.FXML
+    private Label confirmationMessage;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSendRecommendation(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/cse_213/final_project/Mehrab/LaneBreakdownAlertsController.java
@@ -0,0 +1,29 @@
+package cse_213.final_project.Mehrab;
+
+public class LaneBreakdownAlertsController
+{
+    @javafx.fxml.FXML
+    private TextField alertDescriptionField;
+    @javafx.fxml.FXML
+    private Label errorMessage;
+    @javafx.fxml.FXML
+    private Button sendAlertButton;
+    @javafx.fxml.FXML
+    private ComboBox laneComboBox;
+    @javafx.fxml.FXML
+    private DatePicker alertTimePicker;
+    @javafx.fxml.FXML
+    private ComboBox severityComboBox;
+    @javafx.fxml.FXML
+    private TextArea alertDataArea;
+    @javafx.fxml.FXML
+    private Label confirmationMessage;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSendAlert(ActionEvent actionEvent) {
+    }
+}
