98d76087fe903597ce106122f6a5475b7422cd52
anikafima <2321439@iub.edu.bd>
2025-04-15 03:41:12 -0400

    create  .fxml file

--- src/main/java/cse_213/final_project/Fima/Controllers/DailyShiftReportsL2.java
+++
@@ -1,9 +0,0 @@
-package cse_213.final_project.Fima.Controllers;
-
-
-
-public class DailyShiftReports
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/cse_213/final_project/Fima/Controllers/SystemFailuresL3.java
+++
@@ -1,7 +0,0 @@
-package cse_213.final_project.Fima.Controllers;
-
-public class SystemFailures
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
