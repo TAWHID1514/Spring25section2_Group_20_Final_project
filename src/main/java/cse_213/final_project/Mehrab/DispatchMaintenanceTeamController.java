0b85501ea7694bbbaf73926e09f6536e83f191c5
mehrab <mehrabbhuiyan123@gmail.com>
2025-04-16 13:36:04 +0600

    fxml updated

---
+++ src/main/java/cse_213/final_project/Mehrab/DispatchMaintenanceTeamController.java
@@ -0,0 +1,42 @@
+package cse_213.final_project.Mehrab;
+
+public class DispatchMaintenanceTeamController
+{
+    @javafx.fxml.FXML
+    private TextField locationField;
+    @javafx.fxml.FXML
+    private TableColumn locationColumn;
+    @javafx.fxml.FXML
+    private TableColumn priorityLevelColumn;
+    @javafx.fxml.FXML
+    private ComboBox maintenanceTeamComboBox;
+    @javafx.fxml.FXML
+    private Label errorMessage;
+    @javafx.fxml.FXML
+    private ComboBox incidentTypeComboBox;
+    @javafx.fxml.FXML
+    private TextArea descriptionField;
+    @javafx.fxml.FXML
+    private TableColumn maintenanceTeamColumn;
+    @javafx.fxml.FXML
+    private ComboBox priorityLevelComboBox;
+    @javafx.fxml.FXML
+    private DatePicker dispatchDatePicker;
+    @javafx.fxml.FXML
+    private TableView maintenanceTable;
+    @javafx.fxml.FXML
+    private TableColumn incidentTypeColumn;
+    @javafx.fxml.FXML
+    private DatePicker completionDatePicker;
+    @javafx.fxml.FXML
+    private Button dispatchButton;
+    @javafx.fxml.FXML
+    private TableColumn completionDateColumn;
+    @javafx.fxml.FXML
+    private TableColumn dispatchDateColumn;
+    @javafx.fxml.FXML
+    private Label confirmationMessage;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
