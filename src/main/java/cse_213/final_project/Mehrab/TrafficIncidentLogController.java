a0f5691223f528dbf9145041bbab4dee5333617e
mehrab <mehrabbhuiyan123@gmail.com>
2025-04-16 13:15:55 +0600

    log

---
+++ src/main/java/cse_213/final_project/Mehrab/TrafficIncidentLogController.java
@@ -0,0 +1,30 @@
+package cse_213.final_project.Mehrab;
+
+public class TrafficIncidentLogController
+{
+    @javafx.fxml.FXML
+    private TextField locationField;
+    @javafx.fxml.FXML
+    private DatePicker incidentDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox incidentStatusComboBox;
+    @javafx.fxml.FXML
+    private TextArea incidentDescriptionField;
+    @javafx.fxml.FXML
+    private TextField officerNameField;
+    @javafx.fxml.FXML
+    private Label errorMessage;
+    @javafx.fxml.FXML
+    private ComboBox incidentTypeComboBox;
+    @javafx.fxml.FXML
+    private TextArea incidentDataArea;
+    @javafx.fxml.FXML
+    private ComboBox severityLevelComboBox;
+    @javafx.fxml.FXML
+    private Button logIncidentButton;
+    @javafx.fxml.FXML
+    private Label confirmationMessage;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
