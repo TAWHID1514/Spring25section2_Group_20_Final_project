0bf6d7869165c616f44c0e87204d073399c5d6a2
anikafima <2321439@iub.edu.bd>
2025-04-16 00:38:48 -0400

    contoller class

--- src/main/java/cse_213/final_project/Fima/Controllers/Tollcollector.java
+++ src/main/java/cse_213/final_project/Fima/Controllers/Tollcollector.java
@@ -1,23 +1,59 @@
 package cse_213.final_project.Fima.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class Tollcollector
-{
-    @javafx.fxml.FXML
+public class Tollcollector {
+
+    @FXML
+    private TableColumn<?, ?> RegNumtablecolumn;
+
+    @FXML
+    private TableColumn<?, ?> amounttablecolumn;
+
+    @FXML
+    private TextField amounttextfield;
+
+    @FXML
+    private DatePicker datedatepicker;
+
+    @FXML
+    private TableColumn<?, ?> datetablecolumn;
+
+    @FXML
     private TextField regnumtextfield;
-    @javafx.fxml.FXML
-    private TextField nametextfield;
 
-    @javafx.fxml.FXML
-    public void initialize() {
+    @FXML
+    private TableView<?> showdatatableview;
+
+    @FXML
+    private Label showmassagelable;
+
+    @FXML
+    private ComboBox<?> vehicleTypecombobox;
+
+    @FXML
+    private TableColumn<?, ?> vehicleTypetablecolumn;
+
+    @FXML
+    void generateOnActionButton(ActionEvent event) {
+
     }
 
-    @javafx.fxml.FXML
-    public void addOnAction(ActionEvent actionEvent) {
+    @FXML
+    void paymentOnActionButton(ActionEvent event) {
+
     }
 
+    @FXML
+    void retryOnActionButton(ActionEvent event) {
 
+    }
 
 }
--- src/main/java/cse_213/final_project/Fima/Controllers/VipEmergencyVehicles.java
+++ src/main/java/cse_213/final_project/Fima/Controllers/VipEmergencyVehicles.java
@@ -1,7 +1,52 @@
 package cse_213.final_project.Fima.Controllers;
 
-public class VipEmergencyVehicles
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class VipEmergencyVehicles {
+
+    @FXML
+    private Label PriorityPassageLable;
+
+    @FXML
+    private TableColumn<?, ?> PriorityPassagetablecolumn;
+
+    @FXML
+    private TextField RegNumTextfield;
+
+    @FXML
+    private TableColumn<?, ?> RegNumtablecolumn;
+
+    @FXML
+    private TableColumn<?, ?> VehicleModeltablecolumn;
+
+    @FXML
+    private ComboBox<?> VehicleTypecombobox;
+
+    @FXML
+    private TableColumn<?, ?> VehicleTypetablecolumn;
+
+    @FXML
+    private RadioButton noradiobutton;
+
+    @FXML
+    private TableView<?> showdatatableview;
+
+    @FXML
+    private TextField vehiclemodeltextfield;
+
+    @FXML
+    private RadioButton yesradiobutton;
+
+    @FXML
+    void generateOnActionButton(ActionEvent event) {
+
+    }
+
+}
