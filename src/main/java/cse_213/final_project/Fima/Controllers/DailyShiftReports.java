bf7243e72fe50e22e9f9ac5ee110e78f88fd7e5d
anikafima <2321439@iub.edu.bd>
2025-04-16 13:56:33 -0400

    create model class

--- src/main/java/cse_213/final_project/Fima/Controllers/DailyShiftReports.java
+++ src/main/java/cse_213/final_project/Fima/Controllers/DailyShiftReports.java
@@ -1,5 +1,49 @@
-public class DailyShiftReports
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+package cse_213.final_project.Fima.LiaisonOfficer;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DailyShiftReports {
+
+    @FXML
+    private TableColumn<?, ?> BoothIDtablecolumn;
+
+    @FXML
+    private TextField BoothIDtextfield;
+
+    @FXML
+    private TableColumn<?, ?> Issuetablecolumn;
+
+    @FXML
+    private TableColumn<?, ?> TollOperatortablecolumn;
+
+    @FXML
+    private TextField TollOperatortextfield;
+
+    @FXML
+    private TableColumn<?, ?> VehicleCounttablecolumn;
+
+    @FXML
+    private TextArea describeissuetextarea;
+
+    @FXML
+    private TableView<?> showdatatableview;
+
+    @FXML
+    private TextField vehiclecounttextfield;
+
+    @FXML
+    void homeonactionbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitonactionbutton(ActionEvent event) {
+
+    }
+
+}
