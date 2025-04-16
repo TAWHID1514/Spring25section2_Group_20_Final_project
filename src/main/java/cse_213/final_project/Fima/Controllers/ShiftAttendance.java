f1590490e423b00fc124534913c93365ee92c585
anikafima <2321439@iub.edu.bd>
2025-04-16 00:50:04 -0400

    contoller class

--- src/main/java/cse_213/final_project/Fima/Controllers/ShiftAttendance.java
+++ src/main/java/cse_213/final_project/Fima/Controllers/ShiftAttendance.java
@@ -1,7 +1,72 @@
 package cse_213.final_project.Fima.Controllers;
 
-public class ShiftAttendance
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ShiftAttendance {
+
+    @FXML
+    private DatePicker DATEDATEPICKER;
+
+    @FXML
+    private RadioButton DAYRADIOBUTTON;
+
+    @FXML
+    private TableColumn<?, ?> DateTABLECOLUMN;
+
+    @FXML
+    private TextField ENDTIMETEXTFIELD;
+
+    @FXML
+    private TableColumn<?, ?> EndTimeTABLECOLUMN;
+
+    @FXML
+    private TableColumn<?, ?> IDTABLECOLUMN;
+
+    @FXML
+    private TextField IDTEXTFIELD;
+
+    @FXML
+    private RadioButton NIGHTRADIOBUTTON;
+
+    @FXML
+    private TextField OPERATORNAMETEXTFIELD;
+
+    @FXML
+    private TableColumn<?, ?> OperatorNameTABLECOLUMN;
+
+    @FXML
+    private TableView<?> SHOWDATATABLEVIEW;
+
+    @FXML
+    private TextField STARTTIMETEXTFIELD;
+
+    @FXML
+    private TableColumn<?, ?> ShiftTABLECOLUMN;
+
+    @FXML
+    private TableColumn<?, ?> StartTimeTABLECOLUMN;
+
+    @FXML
+    private ComboBox<?> TOLLBOOTHOPERATORCOMBOBOX;
+
+    @FXML
+    private TableColumn<?, ?> TollBoothNumberTABLECOLUMN;
+
+    @FXML
+    void BACKONACTIONBUTTON(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SUBMITONACTIONBUTTON(ActionEvent event) {
+
+    }
+
+}
