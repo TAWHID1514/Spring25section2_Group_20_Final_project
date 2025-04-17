67581ed2bbba7790e85e0ab34f31e33c0636df1a
anikafima <2321439@iub.edu.bd>
2025-04-17 02:51:09 -0400

    create controller class

--- src/main/java/cse_213/final_project/Fima/Controllers/VehicleInformation.java
+++ src/main/java/cse_213/final_project/Fima/Controllers/VehicleInformation.java
@@ -1,7 +1,70 @@
 package cse_213.final_project.Fima.Controllers;
 
-public class VehicleInformation
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class VehicleInformation {
+
+    @FXML
+    private TableColumn<?, ?> Datetablecolumn;
+
+    @FXML
+    private TableColumn<?, ?> OwnerNametablecolumn;
+
+    @FXML
+    private TableColumn<?, ?> RegNumtablecolumn;
+
+    @FXML
+    private Label Regestrationlable;
+
+    @FXML
+    private TableColumn<?, ?> Regestrationtablecolumn;
+
+    @FXML
+    private ComboBox<?> VehicleTollcombobox;
+
+    @FXML
+    private TableColumn<?, ?> VehicleTolltablecolumn;
+
+    @FXML
+    private ComboBox<?> VehicleTypecombobox;
+
+    @FXML
+    private TableColumn<?, ?> VehicleTypetablecolumn;
+
+    @FXML
+    private RadioButton Yesradiobutton;
+
+    @FXML
+    private DatePicker datedatepicker;
+
+    @FXML
+    private RadioButton noradiobutton;
+
+    @FXML
+    private TextField ownernametextfield;
+
+    @FXML
+    private TextField regnumtextfield;
+
+    @FXML
+    private TableView<?> tableview;
+
+    @FXML
+    void backOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOnActionButton(ActionEvent event) {
+
+    }
+
+}
