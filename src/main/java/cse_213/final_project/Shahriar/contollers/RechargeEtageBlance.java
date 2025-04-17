c6a03ca8c04f25d61b0feeee9fb939b9dbd2566c
sayem560 <sayemmr04@gmail.com>
2025-04-18 03:58:54 +0600

    RechargeEtagBalance of user2

---
+++ src/main/java/cse_213/final_project/Shahriar/contollers/RechargeEtageBlance.java
@@ -0,0 +1,31 @@
+package cse_213.final_project.Shahriar.contollers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class RechargeEtageBlance
+{
+    @javafx.fxml.FXML
+    private TextField rechargefxid;
+    @javafx.fxml.FXML
+    private TextField rechargeamountfxid;
+    @javafx.fxml.FXML
+    private ComboBox rechargeMethodfxid;
+    @javafx.fxml.FXML
+    private Label labelStatusfxid;
+    @javafx.fxml.FXML
+    private Label rechargeEtagblancefxid;
+    @javafx.fxml.FXML
+    private AnchorPane Recharge;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void rechargeButtOnonAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/cse_213/final_project/Shahriar/modelclass/RechargeEtagBalance.java
@@ -0,0 +1,35 @@
+package cse_213.final_project.Shahriar.modelclass;
+
+public class RechargeEtagBalance {
+    private String rechargeid;
+    private Double rechargeAmount;
+
+    public RechargeEtagBalance(String rechargeid, Double rechargeAmount) {
+        this.rechargeid = rechargeid;
+        this.rechargeAmount = rechargeAmount;
+    }
+
+    public String getRechargeid() {
+        return rechargeid;
+    }
+
+    public void setRechargeid(String rechargeid) {
+        this.rechargeid = rechargeid;
+    }
+
+    public Double getRechargeAmount() {
+        return rechargeAmount;
+    }
+
+    public void setRechargeAmount(Double rechargeAmount) {
+        this.rechargeAmount = rechargeAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "RechargeEtagBalance{" +
+                "rechargeid='" + rechargeid + '\'' +
+                ", rechargeAmount=" + rechargeAmount +
+                '}';
+    }
+}
