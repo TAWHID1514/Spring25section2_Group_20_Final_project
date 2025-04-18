f8d7d06830436f6edb54c76bf0db959b7694773f
Jarin Tasnim <170942949+mehrab2231348@users.noreply.github.com>
2025-04-18 21:25:45 +0600

    fxml

---
+++ src/main/java/cse_213/final_project/Mehrab/PayTAx.java
@@ -0,0 +1,35 @@
+package cse_213.final_project.Mehrab;
+
+public class PayTAx
+{
+    @javafx.fxml.FXML
+    private TableColumn paymentStatusColumn;
+    @javafx.fxml.FXML
+    private TextField taxAmountField;
+    @javafx.fxml.FXML
+    private TableView taxTableView;
+    @javafx.fxml.FXML
+    private TableColumn paymentDateColumn;
+    @javafx.fxml.FXML
+    private Button payTaxButton;
+    @javafx.fxml.FXML
+    private DatePicker paymentDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn taxTypeColumn;
+    @javafx.fxml.FXML
+    private TableColumn taxAmountColumn;
+    @javafx.fxml.FXML
+    private ComboBox taxTypeComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void handlePayTax(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void PayTaxOA(ActionEvent actionEvent) {
+    }
+}
