5fe2a6422aeb155c13c4ee50487bb20dc92024a5
anikafima <2321439@iub.edu.bd>
2025-04-27 13:24:16 -0400

    update    class

--- src/main/java/cse_213/final_project/LoginFXMLController.java
+++ src/main/java/cse_213/final_project/LoginFXMLController.java
@@ -61,11 +61,11 @@ public class LoginFXMLController
         if ((UsertypeCB.getValue().equals("Police Authority"))){
             SceneSwitcher.switchTo("Tawhid/PoliceAuthurity/Dashboard_PA.fxml");
         }
-        if ((UsertypeCB.getValue().equals("LiaisonOfficerDashboardV2"))){
+        if ((UsertypeCB.getValue().equals("Liaison Officer"))){
             SceneSwitcher.switchTo("Fima/LiaisonOfficer/LiaisonOfficer_Dashboard_V2.fxml");
         }
-        if ((UsertypeCB.getValue().equals("TollBoothOperatorDashboardFima"))){
-            SceneSwitcher.switchTo("TollBoothOperatorDashboard.fxml");
+        if ((UsertypeCB.getValue().equals("Toll booth operator"))){
+            SceneSwitcher.switchTo("Fima/TollBoothOperator/TollBoothOperator_Dashboard_V1.fxml");
         }
 
 
